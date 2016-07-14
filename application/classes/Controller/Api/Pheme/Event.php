2723e6ef9e53f61d32b9b6a67052f85d1acd68a8
David Losada <david@tuxpiper.com>
2016-07-14 10:38:48 +0200

    add pheme event retrieval api route

--- application/classes/Controller/Api/Pheme/Event.php
+++ application/classes/Controller/Api/Pheme/Event.php
@@ -21,12 +21,42 @@ class Controller_Api_Pheme_Event extends Ushahidi_Rest {
 
 	protected $_action_map = array
 	(
+		Http_Request::GET     => 'get',
 		Http_Request::POST    => 'post',
 	);
 
 	protected function _scope()
 	{
 		return 'posts';		# TODO: consider a specific scope for Pheme
+	}
+
+	public function action_get_index_collection()
+	{
+		$req_uri = $this->pheme_config['data-interface']['url'] . '/api/event';
+
+		$response = \Httpful\Request::get($req_uri)
+			->expectsJson()
+			->send();
+
+		if ($response->body->status == "success") {
+			$this->_response_payload = [
+				'count' => count($response->body->data),
+				'results' => $response->body->data
+			];
+		/* TODO: I bet there's more semantic error reporting than this */
+		} else if ($response->body->status == "fail") {
+			$this->_response_payload = [
+				'success' => FALSE,
+				'message' => $response->body->data
+			];
+		} else if ($response->body->status == "error") {
+			$this->_response_payload = [
+				'success' => FALSE,
+				'message' => $response->body->message
+			];
+		}
+
+		$this->_prepare_response();
 	}
 
 	public function action_post_index_collection()
@@ -36,10 +66,11 @@ class Controller_Api_Pheme_Event extends Ushahidi_Rest {
 		$response = \Httpful\Request::post($req_uri)
 			->sendsJson()
 			->body($this->_payload())
+			->expectsJson()
 			->send();
 		$this->_response_payload = [
 			'success' => TRUE,
-			'message' => "got the response"
+			'message' => "Event created"
 		];
 
 		$this->_prepare_response();
